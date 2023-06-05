b327eae8d3365a6ba4ba940898acf3c2555665f3
carloeusebi <carloeusebi@gmail.com>
2023-06-05 15:31:18 +0200

    Added for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
-console.log('JSOK');
+const forButton = document.getElementById('for-print-button');
+const whileButton = document.getElementById('while-print-button');
+
+const listElement = document.getElementById('shopping-list');
+
+const shoppingList = ['milk', 'eggs', 'bread', 'coffe', 'tuna'];
+
+forButton.addEventListener('click', function () {
+
+    let list = '';
+
+    for (let i = 0; i < shoppingList.length; i++) {
+
+        list += `<li>${shoppingList[i]}</li>`;
+    }
+
+    listElement.innerHTML = list;
+})
